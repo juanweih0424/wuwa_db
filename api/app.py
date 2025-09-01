41adb2b52f3710b2b1d511953c7567ea937af028
Juanwei Hu <Juanweih@uci.edu>
2025-08-31 19:35:03 -0700

    fix typo in app.py

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
-from .apiapp import create_app   
-app = create_app()  
+from .apiapp import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
