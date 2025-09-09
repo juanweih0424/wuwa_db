a802d683b0d48e28b244b0a6c179b6fe758f0bd1
Juanwei Hu <Juanweih@uci.edu>
2025-09-09 12:22:28 -0700

    fix landing page in deployement

--- wuwa-db-web/src/components/Layout.jsx
+++ wuwa-db-web/src/components/Layout.jsx
@@ -1,19 +1,16 @@
+import React from "react";
 import Nav from "./Nav.jsx";
 import Footer from "./Footer.jsx";
 import { Outlet } from "react-router-dom";
 
-function Layout(){
-
-    return (
-        <div className="app">
-            <Nav />
-                <main className="main">
-                    <Outlet />  
-                </main>
-            <Footer />
-        </div>
-    );
+export default function Layout() {
+  return (
+    <div className="app">
+      <Nav />
+      <main className="main" role="main">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
-export default Layout;
-    
--- wuwa-db-web/src/main.jsx
+++ wuwa-db-web/src/main.jsx
@@ -1,9 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot, ReactDOM } from 'react-dom/client'
-import './assets/css/global.css'
-import { BrowserRouter } from "react-router-dom";
-import App from './App.jsx'
 import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./assets/css/global.css";
 
 const basename = import.meta.env.MODE === "production" ? "/wuwa_db" : "/";
 
