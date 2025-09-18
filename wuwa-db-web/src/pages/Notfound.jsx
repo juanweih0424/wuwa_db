3b98391003c3f3b6487a309c28d7047d32bc7741
Juanwei Hu <Juanweih@uci.edu>
2025-09-18 04:33:57 -0700

    fix layout in 404 page for all devicess

--- wuwa-db-web/src/pages/Notfound.jsx
+++ wuwa-db-web/src/pages/Notfound.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 function Notfound(){
     
     return (
-        <div className="flex min-h-full flex-col justify-center items-center gap-6 px-4 py-8"> 
+        <div className="flex min-h-full min-w-full flex-col justify-center items-center gap-6 px-4 py-8"> 
             <img src={roverdrag} className="w-50 h-50"/>
             <p className="font-bold text-3xl text-center">oops ... 404 Not Found</p>
             <p className="font-base text-xl text-center">The page you are looking for does not exist or an error occurred</p>
